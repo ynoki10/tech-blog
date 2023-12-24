690ac85784820f96628a06b943f0d19971619d15
ynoki10 <sakanafront@gmail.com>
2023-12-24 11:19:59 +0900

    フッター調整

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,22 +4,20 @@ import siteMetadata from '@/data/siteMetadata';
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="mt-16 flex flex-col items-center">
-        <ul className="mb-3 flex space-x-4">
-          <li>
-            <SocialIcon kind="github" href={siteMetadata.github} />
-          </li>
-          <li>
-            <SocialIcon kind="twitter" href={siteMetadata.twitter} />
-          </li>
-        </ul>
-        <p className="mb-2 flex space-x-2 text-sm text-gray-500">
-          <span>{siteMetadata.author}</span>
-          <span>{`© ${new Date().getFullYear()}`}</span>
-          <span>{siteMetadata.title}</span>
-        </p>
-      </div>
+    <footer className="mt-12 flex flex-col items-center border-t py-4">
+      <ul className="flex space-x-4">
+        <li>
+          <SocialIcon kind="github" href={siteMetadata.github} />
+        </li>
+        <li>
+          <SocialIcon kind="twitter" href={siteMetadata.twitter} />
+        </li>
+      </ul>
+      <p className="mt-3 flex space-x-2 text-sm text-gray-500">
+        <span>{siteMetadata.author}</span>
+        <span>{`© ${new Date().getFullYear()}`}</span>
+        <span>{siteMetadata.title}</span>
+      </p>
     </footer>
   );
 }
