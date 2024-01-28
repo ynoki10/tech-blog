41dc764010da237629396797c1ba79340048d25f
ynoki10 <sakanafront@gmail.com>
2024-01-28 15:08:36 +0900

    プロフィール更新

--- data/siteMetadata.ts
+++ data/siteMetadata.ts
@@ -5,7 +5,7 @@ const siteMetadata = {
   description:
     'Yoshinokiの技術ブログです。主にフロントエンドの技術やアクセシビリティについて、調べたことや学んだことを雑多にまとめています。',
   profile:
-    'マークアップ&フロントエンドエンジニア。Web制作会社でコーポレートサイトなど作っています。今はアクセシビリティとJamstackを勉強中。95年生まれ。京都に住んでいます🦆',
+    'マークアップ&フロントエンドエンジニア。Web制作会社でコーポレートサイトなど作っています。今はアクセシビリティとJamstackを勉強中。京都に住んでいます🦆',
   language: 'ja-jp',
   theme: 'system',
   siteUrl: 'https:example.com',
