cb8e18cef61280201ee242d01287d56d21e44000
ynoki10 <sakanafront@gmail.com>
2024-01-30 21:34:09 +0900

    記事執筆:11ty(eleventy)でファイル名のままhtmlを出力する方法

--- data/tags.ts
+++ data/tags.ts
@@ -1,4 +1,7 @@
-export const TAGS = [{ label: '雑記', slug: 'random-note' }] as const;
+export const TAGS = [
+  { label: '雑記', slug: 'random-note' },
+  { label: '11ty', slug: '11ty' },
+] as const;
 
 export type Tag = (typeof TAGS)[number];
 export type TagWithCount = Tag & { count: number };
