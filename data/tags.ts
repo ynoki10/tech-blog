dff94a6041be4477f30ae6d9385f59850f460c79
ynoki10 <sakanafront@gmail.com>
2024-02-01 21:36:06 +0900

    記事執筆: Splideの読み上げラベルを日本語化する

--- data/tags.ts
+++ data/tags.ts
@@ -1,6 +1,9 @@
 export const TAGS = [
   { label: '雑記', slug: 'random-note' },
   { label: '11ty', slug: '11ty' },
+  { label: 'Tips', slug: 'tips' },
+  { label: 'A11y', slug: 'a11y' },
+  { label: 'Splide', slug: 'splide' },
 ] as const;
 
 export type Tag = (typeof TAGS)[number];
