fdf1c4f40df3a31c020bbb5cb1678ab988247522
ynoki10 <sakanafront@gmail.com>
2024-02-11 17:15:01 +0900

    next lint の対象ディレクトリを設定

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    dirs: ['app', 'components', 'data'],
+  },
+};
 
 module.exports = withContentlayer(nextConfig);
