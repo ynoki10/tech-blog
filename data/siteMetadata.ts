d9a0653dcd6585daf9694237b6b4f304b8bbb404
ynoki10 <sakanafront@gmail.com>
2024-07-05 21:56:36 +0900

    Update profile

--- data/siteMetadata.ts
+++ data/siteMetadata.ts
@@ -1,20 +1,20 @@
 const siteMetadata = {
-  title: "Yoshinoki's Blog",
-  author: 'Yoshinoki',
-  headerTitle: "Yoshinoki's Blog",
-  description:
-    'Yoshinokiの技術ブログです。主にフロントエンドの技術やアクセシビリティについて、調べたことや学んだことを雑多にまとめています。',
-  profile:
-    'マークアップ&フロントエンドエンジニア。Web制作会社で主にコーポレートサイトを作っています。最近はアクセシビリティとモダンフロントエンド、Jamstackなどを勉強中。京都に住んでいます🦆',
-  language: 'ja-jp',
-  siteUrl: 'https://www.yoshinoki.dev',
-  siteRepo: 'https://github.com/ynoki10/tech-blog',
-  siteLogo: '/static/images/logo.png',
-  github: 'https://github.com/ynoki10',
-  twitter: 'https://twitter.com/4noki10',
-  twitterId: '@4noki10',
-  zenn: 'https://zenn.dev/yoshinoki',
-  locale: 'ja-JP',
+	title: "Yoshinoki's Blog",
+	author: 'Yoshinoki',
+	headerTitle: "Yoshinoki's Blog",
+	description:
+		'Yoshinokiの技術ブログです。主にフロントエンドの技術やアクセシビリティについて、調べたことや学んだことを雑多にまとめています。',
+	profile:
+		'マークアップ&フロントエンドエンジニア。最近はアクセシビリティとFigma、Astroなどを勉強中。京都に住んでいます🦆',
+	language: 'ja-jp',
+	siteUrl: 'https://www.yoshinoki.dev',
+	siteRepo: 'https://github.com/ynoki10/tech-blog',
+	siteLogo: '/static/images/logo.png',
+	github: 'https://github.com/ynoki10',
+	twitter: 'https://twitter.com/4noki10',
+	twitterId: '@4noki10',
+	zenn: 'https://zenn.dev/yoshinoki',
+	locale: 'ja-JP',
 } as const;
 
 export default siteMetadata;
